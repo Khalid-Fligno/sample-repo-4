bda206d82a647ea422eb48349b56f1f0fa4a26b2
Caleb Harnell <calebharnell@hotmail.com>
2018-05-30 15:19:49 +1000

    Cache fonts and image in AuthLoading

--- src/screens/AuthLoadingScreen.js
+++ src/screens/AuthLoadingScreen.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { AppLoading, Asset, Font } from 'expo';
+
+const cacheImages = (images) => {
+  return images.map((image) => {
+    return Asset.fromModule(image).downloadAsync();
+  });
+};
+
+const cacheFonts = (fonts) => {
+  return fonts.map((font) => {
+    return Font.loadAsync(font);
+  });
+};
 
 export default class AuthLoadingScreen extends React.PureComponent {
+  loadAssetsAsync = async () => {
+    const imageAssets = cacheImages([
+      require('../../assets/images/yazzy-colour-mask.png'),
+    ]);
+    const fontAssets = cacheFonts([
+      {
+        GothamBold: require('../../assets/fonts/gotham-bold.otf'),
+      },
+      {
+        GothamBook: require('../../assets/fonts/gotham-book.otf'),
+      },
+      {
+        GothamNarrowLight: require('../../assets/fonts/gotham-narrow-light.otf'),
+      },
+    ]);
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
+  cachingComplete = async () => {
+    this.props.navigation.navigate('App');
+  }
   render() {
     return (
-      <View
-        style={styles.container}
-      >
-        <Text>AuthLoadingScreen</Text>
-      </View>
+      <AppLoading
+        startAsync={this.loadAssetsAsync}
+        onFinish={() => this.cachingComplete()}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
