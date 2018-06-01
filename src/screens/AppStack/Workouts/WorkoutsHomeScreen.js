b1911875d97a2da9d60903f258beb51d996c29f3
Caleb Harnell <calebharnell@hotmail.com>
2018-06-01 10:01:55 +1000

    Change background color

--- src/screens/AppStack/Workouts/WorkoutsHomeScreen.js
+++ src/screens/AppStack/Workouts/WorkoutsHomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import colors from '../../../styles/colors';
 
 export default class WorkoutsHomeScreen extends React.PureComponent {
   constructor(props) {
@@ -21,7 +22,7 @@ export default class WorkoutsHomeScreen extends React.PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.offWhite,
     alignItems: 'center',
     justifyContent: 'center',
   },
