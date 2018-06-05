a522dd3b1621333821f5daac37d67cb2f5736939
Caleb Harnell <calebharnell@hotmail.com>
2018-06-05 14:31:38 +1000

    Add firebase

--- src/screens/AppStack/Nutrition/NutritionHomeScreen.js
+++ src/screens/AppStack/Nutrition/NutritionHomeScreen.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import { StyleSheet, View, Text, ScrollView } from 'react-native';
-import CustomButton from '../../../components/CustomButton';
+// import CustomButton from '../../../components/CustomButton';
 import colors from '../../../styles/colors';
+import firebase from '../../../../config/firebase';
 
 export default class NutritionHomeScreen extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      recipes: [],
     };
   }
+  componentWillMount = async () => {
+    await this.fetchRecipes();
+  }
+  fetchRecipes = async () => {
+    await firebase.database().ref('recipes/').once('value', (snapshot) => {
+      const recipes = snapshot.val();
+      this.setState({
+        recipes,
+      });
+    });
+  }
   render() {
+    const recipeList = Object.keys(this.state.recipes).map((recipe) => (
+      <Text key={recipe}>{recipe}</Text>
+    ));
+    const placeholder = <Text>No recipes</Text>;
     return (
       <View style={styles.container}>
         <Text>
           NutritionHomeScreen
         </Text>
+        {
+          recipeList || placeholder
+        }
         <ScrollView>
-          <View
+          {/* <View
             style={{
               paddingTop: 10,
               paddingBottom: 10,
@@ -178,7 +198,7 @@ export default class NutritionHomeScreen extends React.PureComponent {
               outline
               disabled
             />
-          </View>
+          </View> */}
         </ScrollView>
       </View>
     );
