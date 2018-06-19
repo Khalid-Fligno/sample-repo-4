fdd1ed00e81a6604c1ba318ee420f4f6f792fd5d
Caleb Harnell <calebharnell@hotmail.com>
2018-06-19 12:36:59 +1000

    Add extra step images to example recipe

--- src/utils/recipeScreenImageMap.js
+++ src/utils/recipeScreenImageMap.js
@@ -3,6 +3,8 @@ const recipeScreenImageMap = {
   friedChicken1: require('../../assets/images/recipes/baked-eggs-1024x768.png'),
   friedChicken2: require('../../assets/images/recipes/baked-eggs-1024x768.png'),
   friedChicken3: require('../../assets/images/recipes/baked-eggs-1024x768.png'),
+  friedChicken4: require('../../assets/images/recipes/baked-eggs-1024x768.png'),
+  friedChicken5: require('../../assets/images/recipes/baked-eggs-1024x768.png'),
 };
 
 export default recipeScreenImageMap;
