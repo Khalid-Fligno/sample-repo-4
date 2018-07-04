97b4ad784b15b38211701cf1a30e6dc620adaf35
Caleb Harnell <calebharnell@hotmail.com>
2018-07-04 11:32:07 +1000

    Create custom countdown timer component

---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import fonts from '../styles/fonts';
+import colors from '../styles/colors';
+
+export default class CountdownTimer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remainingTime: (props.totalDuration * 1000) + 900,
+    };
+  }
+  componentDidMount() {
+    if (this.props.start) {
+      this.start();
+    }
+  }
+  componentWillReceiveProps(newProps) {
+    if (newProps.start) {
+      this.start();
+    } else {
+      this.stop();
+    }
+    if (newProps.reset) {
+      this.reset(newProps.totalDuration);
+    }
+  }
+  start = () => {
+    const { handleFinish } = this.props;
+    const endTime = new Date().getTime() + this.state.remainingTime;
+    this.interval = setInterval(() => {
+      const remaining = endTime - new Date();
+      if (remaining <= 1000) {
+        this.setState({ remainingTime: 0 });
+        this.stop();
+        handleFinish();
+        return;
+      }
+      this.setState({ remainingTime: remaining });
+    }, 1);
+  }
+  stop = () => {
+    clearInterval(this.interval);
+  }
+  reset = (newDuration) => {
+    this.setState({
+      remainingTime: this.props.totalDuration !== newDuration ? newDuration : this.props.totalDuration,
+    });
+  }
+  formatTime = () => {
+    const now = this.state.remainingTime;
+    const seconds = Math.floor(now / 1000);
+    const formatted = `${seconds}`;
+    if (typeof this.props.getTime === 'function') {
+      this.props.getTime(formatted);
+    }
+    return formatted;
+  }
+  render() {
+    const styles = this.props.options ? this.props.options : defaultStyles;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.formatTime()}</Text>
+      </View>
+    );
+  }
+}
+
+const defaultStyles = {
+  container: {
+    backgroundColor: colors.white,
+    paddingTop: 5,
+    paddingRight: 5,
+    paddingBottom: 0,
+    paddingLeft: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: fonts.bold,
+    fontSize: 72,
+    color: colors.charcoal.standard,
+  },
+};
+
+CountdownTimer.propTypes = {
+  totalDuration: PropTypes.number.isRequired,
+  start: PropTypes.bool.isRequired,
+  options: PropTypes.objectOf(PropTypes.string || PropTypes.number),
+  getTime: PropTypes.func,
+  handleFinish: PropTypes.func.isRequired,
+};
+
+CountdownTimer.defaultProps = {
+  getTime: null,
+  options: null,
+};
