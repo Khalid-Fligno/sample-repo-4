e938866d5e32673d7112e14db39057aaff0166c3
Caleb Harnell <calebharnell@hotmail.com>
2018-07-05 13:13:15 +1000

    Fetch pedometer info

--- src/screens/AppStack/Profile/ProfileHomeScreen.js
+++ src/screens/AppStack/Profile/ProfileHomeScreen.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Pedometer } from 'expo';
 import colors from '../../../styles/colors';
 
 export default class ProfileHomeScreen extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      stepCount: 0,
     };
   }
+  componentDidMount() {
+    this.getPedometerInfo();
+  }
+  getPedometerInfo = () => {
+    try {
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - 1);
+      Pedometer.getStepCountAsync(start, end).then((result) => {
+        this.setState({ stepCount: result.steps });
+      });
+    } catch (err) {
+      this.setState({ stepCount: 'Pedometer info not available' });
+    }
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text>
           ProfileHomeScreen
         </Text>
+        <Text>Walk! And watch this go up: {this.state.stepCount}</Text>
       </View>
     );
   }
