b2581538cbde19b36878c050c8340e0f764e0aba
Caleb Harnell <calebharnell@hotmail.com>
2018-07-11 11:54:41 +1000

    Loading state and unsubscribe db on unmount

--- src/screens/AppStack/Workouts/WorkoutsSelectionScreen.js
+++ src/screens/AppStack/Workouts/WorkoutsSelectionScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { db } from '../../../../config/firebase';
+import Loader from '../../../components/Loader';
 import colors from '../../../styles/colors';
 
 export default class WorkoutsSelectionScreen extends React.PureComponent {
@@ -13,24 +14,33 @@ export default class WorkoutsSelectionScreen extends React.PureComponent {
   componentDidMount = () => {
     this.fetchWorkouts();
   }
-  fetchWorkouts = () => {
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  fetchWorkouts = async () => {
+    this.setState({ loading: true });
     const type = this.props.navigation.getParam('workoutType', null);
     const location = this.props.navigation.getParam('workoutLocation', null);
-    db.collection('workouts')
-      .where('type', '==', type)
-      .where('location', '==', location)
-      .onSnapshot((querySnapshot) => {
-        const workouts = [];
-        querySnapshot.forEach((doc) => {
-          workouts.push(doc.data());
+    try {
+      this.unsubscribe = db.collection('workouts')
+        .where('type', '==', type)
+        .where('location', '==', location)
+        .onSnapshot((querySnapshot) => {
+          const workouts = [];
+          querySnapshot.forEach((doc) => {
+            workouts.push(doc.data());
+          });
+          this.setState({
+            workouts,
+            loading: false,
+          });
         });
-        this.setState({
-          workouts,
-        });
-      });
+    } catch (err) {
+      this.setState({ loading: false });
+    }
   }
   render() {
-    const { workouts } = this.state;
+    const { workouts, loading } = this.state;
     const { navigate, getParam } = this.props.navigation;
     const workoutType = getParam('workoutType', null);
     const workoutLocation = getParam('workoutLocation', null);
@@ -42,6 +52,14 @@ export default class WorkoutsSelectionScreen extends React.PureComponent {
         {workout.name}
       </Text>
     ));
+    if (loading) {
+      return (
+        <Loader
+          loading={loading}
+          color={colors.coral.standard}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <Text>
