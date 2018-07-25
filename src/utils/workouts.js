2baa2ff5be7fae35e3a3e9345e7b2773394e6684
Caleb Harnell <calebharnell@hotmail.com>
2018-07-25 14:35:42 +1000

    Refactor functions from workout stack

---
+++ src/utils/workouts.js
@@ -0,0 +1,23 @@
+export const findFocus = (workoutObject) => {
+  if (workoutObject && workoutObject.upperBody) {
+    return 'Upper Body';
+  } else if (workoutObject && workoutObject.lowerBody) {
+    return 'Lower Body';
+  } else if (workoutObject && workoutObject.fullBody) {
+    return 'Full Body';
+  } else if (workoutObject && workoutObject.core) {
+    return 'Core';
+  }
+  return null;
+};
+
+export const findLocation = (workoutObject) => {
+  if (workoutObject && workoutObject.gym) {
+    return 'Gym';
+  } else if (workoutObject && workoutObject.home) {
+    return 'Home';
+  } else if (workoutObject && workoutObject.park) {
+    return 'Park';
+  }
+  return null;
+};
