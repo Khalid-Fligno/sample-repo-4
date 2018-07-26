349f0316e23a7cfa6471316b0f35bae233629b16
Caleb Harnell <calebharnell@hotmail.com>
2018-07-26 11:41:24 +1000

    Add styled calendar strip to calndar home screen

--- src/screens/AppStack/Calendar/CalendarHomeScreen.js
+++ src/screens/AppStack/Calendar/CalendarHomeScreen.js
@@ -1,19 +1,74 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import CalendarStrip from 'react-native-calendar-strip';
+import Icon from '../../../components/Icon';
 import colors from '../../../styles/colors';
+import fonts from '../../../styles/fonts';
 
 export default class CalendarHomeScreen extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
     };
+    this.calendarStrip = React.createRef();
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          CalendarHomeScreen
-        </Text>
+        <CalendarStrip
+          ref={this.calendarStrip}
+          calendarAnimation={{
+            type: 'parallel',
+            duration: 500,
+          }}
+          daySelectionAnimation={{
+            type: 'background',
+            duration: 400,
+            highlightColor: colors.green.standard,
+          }}
+          style={{
+            height: 90,
+            paddingTop: 10,
+            paddingBottom: 20,
+          }}
+          calendarHeaderStyle={{
+            fontFamily: fonts.bold,
+            color: colors.white,
+            marginTop: 0,
+            marginBottom: 15,
+          }}
+          calendarColor={colors.green.dark}
+          dateNumberStyle={{
+            fontFamily: fonts.bold,
+            color: colors.white,
+          }}
+          dateNameStyle={{
+            fontFamily: fonts.bold,
+            color: colors.white,
+          }}
+          highlightDateNumberStyle={{
+            fontFamily: fonts.bold,
+            color: colors.charcoal.dark,
+          }}
+          highlightDateNameStyle={{
+            fontFamily: fonts.bold,
+            color: colors.charcoal.dark,
+          }}
+          weekendDateNameStyle={{
+            fontFamily: fonts.bold,
+            color: colors.grey.standard,
+          }}
+          weekendDateNumberStyle={{
+            fontFamily: fonts.bold,
+            color: colors.grey.standard,
+          }}
+          iconContainer={{
+            flex: 0.15,
+          }}
+          leftSelector={<Icon name="chevron-left" size={20} color={colors.charcoal.standard} />}
+          rightSelector={<Icon name="chevron-right" size={20} color={colors.charcoal.standard} />}
+        />
       </View>
     );
   }
@@ -23,7 +78,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
