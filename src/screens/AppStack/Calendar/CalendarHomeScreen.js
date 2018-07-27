bd856ec28ab1e852db79b7384cdf83f9f65b2317
Caleb Harnell <calebharnell@hotmail.com>
2018-07-27 12:43:46 +1000

    Add tiles to calendar home screen

--- src/screens/AppStack/Calendar/CalendarHomeScreen.js
+++ src/screens/AppStack/Calendar/CalendarHomeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { StyleSheet, View, AsyncStorage, ActivityIndicator } from 'react-native';
 import CalendarStrip from 'react-native-calendar-strip';
+import { db } from '../../../../config/firebase';
+import CalendarTile from '../../../components/CalendarTile';
 import Icon from '../../../components/Icon';
 import colors from '../../../styles/colors';
 import fonts from '../../../styles/fonts';
@@ -10,14 +11,73 @@ export default class CalendarHomeScreen extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      workout: undefined,
+      breakfast: undefined,
+      lunch: undefined,
+      dinner: undefined,
+      snack: undefined,
+      loading: false,
     };
     this.calendarStrip = React.createRef();
   }
+  componentDidMount = async () => {
+    this.setState({ loading: true });
+    const uid = await AsyncStorage.getItem('uid');
+    const stringDate = this.calendarStrip.current.getSelectedDate().format('YYYY-MM-DD').toString();
+    await db.collection('users').doc(uid)
+      .collection('calendarEntries').doc(stringDate)
+      .get()
+      .then(async (doc) => {
+        if (doc.exists) {
+          this.setState({
+            workout: await doc.data().workout,
+            breakfast: await doc.data().breakfast,
+            lunch: await doc.data().lunch,
+            dinner: await doc.data().dinner,
+            snack: await doc.data().snack,
+            loading: false,
+          });
+        } else {
+          this.setState({ loading: false });
+        }
+      });
+  }
+  handleDateSelected = async (date) => {
+    this.setState({ loading: true });
+    const uid = await AsyncStorage.getItem('uid');
+    const stringDate = date.format('YYYY-MM-DD').toString();
+    await db.collection('users').doc(uid)
+      .collection('calendarEntries').doc(stringDate)
+      .get()
+      .then(async (doc) => {
+        if (doc.exists) {
+          this.setState({
+            workout: await doc.data().workout,
+            breakfast: await doc.data().breakfast,
+            lunch: await doc.data().lunch,
+            dinner: await doc.data().dinner,
+            snack: await doc.data().snack,
+            loading: false,
+          });
+        } else {
+          this.setState({
+            workout: undefined,
+            breakfast: undefined,
+            lunch: undefined,
+            dinner: undefined,
+            snack: undefined,
+            loading: false,
+          });
+        }
+      });
+  }
   render() {
+    const { loading } = this.state;
     return (
       <View style={styles.container}>
         <CalendarStrip
           ref={this.calendarStrip}
+          onDateSelected={(date) => this.handleDateSelected(date)}
           calendarAnimation={{
             type: 'parallel',
             duration: 500,
@@ -69,6 +129,34 @@ export default class CalendarHomeScreen extends React.PureComponent {
           leftSelector={<Icon name="chevron-left" size={20} color={colors.charcoal.standard} />}
           rightSelector={<Icon name="chevron-right" size={20} color={colors.charcoal.standard} />}
         />
+        {
+          loading ?
+            (
+              <View
+                style={{
+                  flexGrow: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <ActivityIndicator
+                  size="large"
+                />
+              </View>
+            ) : (
+              <View
+                style={{
+                  flexGrow: 1,
+                }}
+              >
+                <CalendarTile entry={this.state.workout} />
+                <CalendarTile entry={this.state.breakfast} />
+                <CalendarTile entry={this.state.lunch} />
+                <CalendarTile entry={this.state.dinner} />
+                <CalendarTile entry={this.state.snack} />
+              </View>
+            )
+        }
       </View>
     );
   }
