eb77effe124ce7aaeea07db2108078624ea2bd04
Caleb Harnell <calebharnell@hotmail.com>
2018-08-01 10:26:41 +1000

    Display and link to workouts/recipes from calendar

--- src/screens/AppStack/Calendar/CalendarHomeScreen.js
+++ src/screens/AppStack/Calendar/CalendarHomeScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, AsyncStorage, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, AsyncStorage, ActivityIndicator, Dimensions, TouchableOpacity, Text } from 'react-native';
+import { FileSystem } from 'expo';
 import CalendarStrip from 'react-native-calendar-strip';
-import { db } from '../../../../config/firebase';
-import CalendarTile from '../../../components/CalendarTile';
+import { db, auth } from '../../../../config/firebase';
 import Icon from '../../../components/Icon';
+import { findReps } from '../../../utils';
 import colors from '../../../styles/colors';
 import fonts from '../../../styles/fonts';
+
+const { width } = Dimensions.get('window');
 
 export default class CalendarHomeScreen extends React.PureComponent {
   constructor(props) {
@@ -71,8 +74,184 @@ export default class CalendarHomeScreen extends React.PureComponent {
         }
       });
   }
+  loadExercises = async (workout) => {
+    this.setState({ loading: true });
+    const user = auth.currentUser;
+    let reps;
+    if (user) {
+      db.collection('users')
+        .doc(user.uid)
+        .get()
+        .then(async (doc) => {
+          if (doc.exists) {
+            reps = findReps(await doc.data().fitnessLevel);
+          }
+        });
+    }
+    const { exercises } = workout;
+    try {
+      await Promise.all(exercises.map(async (exercise, index) => {
+        await FileSystem.downloadAsync(
+          exercise.videoURL,
+          `${FileSystem.cacheDirectory}exercise-${index + 1}.mp4`,
+        );
+      }));
+      this.setState({ loading: false });
+      this.props.navigation.push('WorkoutInfo', { workout, reps });
+    } catch (err) {
+      console.log(`Filesystem download error: ${err}`);
+    }
+  }
   render() {
-    const { loading } = this.state;
+    const {
+      loading,
+      workout,
+      breakfast,
+      lunch,
+      dinner,
+      snack,
+    } = this.state;
+    const loadingView = (
+      <View style={styles.loadingViewContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+    const dayDisplay = (
+      <View style={styles.dayDisplayContainer}>
+        <Text>Workout</Text>
+        {
+          workout ? (
+            <TouchableOpacity
+              onPress={() => this.loadExercises(workout)}
+              style={styles.workoutTile}
+            >
+              <Text style={styles.tileText}>
+                {workout && workout.name}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('WorkoutsHome')}
+              style={styles.blankTile}
+            >
+              <Icon
+                name="add-circle"
+                size={22}
+                color={colors.grey.standard}
+              />
+              <Text style={styles.blankTileText}>
+                Add a workout
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+        <Text>Meals</Text>
+        {
+          breakfast ? (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('Recipe', { recipe: breakfast })}
+              style={styles.nutritionTile}
+            >
+              <Text style={styles.tileText}>
+                {breakfast && breakfast.title}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('NutritionHome')}
+              style={styles.blankTile}
+            >
+              <Icon
+                name="add-circle"
+                size={22}
+                color={colors.grey.standard}
+              />
+              <Text style={styles.blankTileText}>
+                Add a breakfast recipe
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+        {
+          lunch ? (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('Recipe', { recipe: lunch })}
+              style={styles.nutritionTile}
+            >
+              <Text style={styles.tileText}>
+                {lunch && lunch.title}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('NutritionHome')}
+              style={styles.blankTile}
+            >
+              <Icon
+                name="add-circle"
+                size={22}
+                color={colors.grey.standard}
+              />
+              <Text style={styles.blankTileText}>
+                Add a lunch recipe
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+        {
+          dinner ? (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('Recipe', { recipe: dinner })}
+              style={styles.nutritionTile}
+            >
+              <Text style={styles.tileText}>
+                {dinner && dinner.title}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('NutritionHome')}
+              style={styles.blankTile}
+            >
+              <Icon
+                name="add-circle"
+                size={22}
+                color={colors.grey.standard}
+              />
+              <Text style={styles.blankTileText}>
+                Add a dinner recipe
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+        {
+          snack ? (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('Recipe', { recipe: snack })}
+              style={styles.nutritionTile}
+            >
+              <Text style={styles.tileText}>
+                {snack && snack.title}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push('NutritionHome')}
+              style={styles.blankTile}
+            >
+              <Icon
+                name="add-circle"
+                size={22}
+                color={colors.grey.standard}
+              />
+              <Text style={styles.blankTileText}>
+                Add a snack recipe
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+      </View>
+    );
     return (
       <View style={styles.container}>
         <CalendarStrip
@@ -129,34 +308,7 @@ export default class CalendarHomeScreen extends React.PureComponent {
           leftSelector={<Icon name="chevron-left" size={20} color={colors.charcoal.standard} />}
           rightSelector={<Icon name="chevron-right" size={20} color={colors.charcoal.standard} />}
         />
-        {
-          loading ?
-            (
-              <View
-                style={{
-                  flexGrow: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <ActivityIndicator
-                  size="large"
-                />
-              </View>
-            ) : (
-              <View
-                style={{
-                  flexGrow: 1,
-                }}
-              >
-                <CalendarTile entry={this.state.workout} />
-                <CalendarTile entry={this.state.breakfast} />
-                <CalendarTile entry={this.state.lunch} />
-                <CalendarTile entry={this.state.dinner} />
-                <CalendarTile entry={this.state.snack} />
-              </View>
-            )
-        }
+        {loading ? loadingView : dayDisplay}
       </View>
     );
   }
@@ -166,5 +318,76 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+  },
+  loadingViewContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dayDisplayContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    paddingTop: 7.5,
+    paddingBottom: 7.5,
+  },
+  workoutTile: {
+    opacity: 0.9,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width - 30,
+    marginTop: 7.5,
+    marginBottom: 7.5,
+    backgroundColor: colors.coral.standard,
+    borderRadius: 4,
+    shadowColor: colors.black,
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+  },
+  nutritionTile: {
+    opacity: 0.9,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width - 30,
+    marginTop: 7.5,
+    marginBottom: 7.5,
+    backgroundColor: colors.violet.standard,
+    borderRadius: 4,
+    shadowColor: colors.black,
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+  },
+  tileText: {
+    fontFamily: fonts.boldItalic,
+    fontSize: 24,
+    color: colors.white,
+    textAlign: 'center',
+  },
+  blankTile: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width - 30,
+    marginTop: 7.5,
+    marginBottom: 7.5,
+    backgroundColor: colors.offWhite,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: colors.grey.light,
+    shadowColor: colors.black,
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+  },
+  blankTileText: {
+    marginTop: 4,
+    marginLeft: 10,
+    fontFamily: fonts.standard,
+    fontSize: 20,
+    color: colors.grey.standard,
   },
 });
