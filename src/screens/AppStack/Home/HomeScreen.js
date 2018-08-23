8213be5062e24b651d4e5bf43b64d6f262c04475
Caleb Harnell <calebharnell@hotmail.com>
2018-08-23 11:58:29 +1000

    Update homescreen as per designs

--- src/screens/AppStack/Home/HomeScreen.js
+++ src/screens/AppStack/Home/HomeScreen.js
@@ -4,9 +4,16 @@ import {
   View,
   Linking,
   ScrollView,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
 } from 'react-native';
 import NewsFeedTile from '../../../components/NewsFeedTile';
 import colors from '../../../styles/colors';
+import fonts from '../../../styles/fonts';
+
+const { width } = Dimensions.get('window');
 
 export default class HomeScreen extends React.PureComponent {
   constructor(props) {
@@ -18,29 +25,47 @@ export default class HomeScreen extends React.PureComponent {
     return (
       <View style={styles.container}>
         <ScrollView contentContainerStyle={styles.scrollView}>
-          <NewsFeedTile
-            image={require('../../../../assets/images/fitazfk-blog.jpg')}
-            title="FitazFK Blog"
-            subTitle="Ground Bean Tacos with Avocado Cream"
-            onPress={() => Linking.openURL('https://www.fitazfkblog.com/home/2018/7/3/the-18-min-workout-that-will-transform-your-life')}
-          />
+          <View style={styles.doubleTileContainer}>
+            <TouchableOpacity
+              onPress={() => Linking.openURL('https://www.fitazfkblog.com/home/2018/7/3/the-18-min-workout-that-will-transform-your-life')}
+              style={styles.cardContainer}
+            >
+              <ImageBackground
+                source={require('../../../../assets/images/fitazfk-blog.jpg')}
+                style={styles.image}
+              >
+                <View style={styles.titleContainer}>
+                  <Text style={styles.title}>
+                    CHECK OUT SOME TACOS
+                  </Text>
+                </View>
+              </ImageBackground>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => Linking.openURL('https://www.fitazfkblog.com/home/2018/7/3/the-18-min-workout-that-will-transform-your-life')}
+              style={styles.cardContainer}
+            >
+              <ImageBackground
+                source={require('../../../../assets/images/fitazfk-blog.jpg')}
+                style={styles.image}
+              >
+                <View style={styles.titleContainer}>
+                  <Text style={styles.title}>
+                    CHECK OUT SOME TACOS
+                  </Text>
+                </View>
+              </ImageBackground>
+            </TouchableOpacity>
+          </View>
           <NewsFeedTile
             image={require('../../../../assets/images/fitazfk-facebook-group.jpg')}
             title="Join the FitazFK Army"
-            subTitle="Share progress and trade tips and tricks for following our guides"
             onPress={() => Linking.openURL('https://www.facebook.com/groups/180007149128432/?source_id=204363259589572')}
           />
           <NewsFeedTile
             image={require('../../../../assets/images/fitazfk-shop-now.jpg')}
-            title="Buy Now"
-            subTitle="Browse our range of fitness equipment"
+            title="Shop Bundles Online"
             onPress={() => Linking.openURL('https://fitazfk.com/collections/equipment')}
-          />
-          <NewsFeedTile
-            image={require('../../../../assets/images/fitazfk-shop-now.jpg')}
-            title="Keto Glo"
-            subTitle="Coming Soon"
-            onPress={() => Linking.openURL('https://www.fitazfkblog.com/home/2018/7/3/the-18-min-workout-that-will-transform-your-life')}
           />
         </ScrollView>
       </View>
@@ -51,9 +76,41 @@ export default class HomeScreen extends React.PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.white,
+    backgroundColor: colors.offWhite,
   },
   scrollView: {
-    paddingBottom: 15,
+    flex: 1,
+    padding: 5,
+  },
+  doubleTileContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  cardContainer: {
+    flex: 1,
+    margin: 5,
+    shadowColor: colors.grey.dark,
+    shadowOpacity: 0.6,
+    shadowOffset: { width: 0, height: -2 },
+    shadowRadius: 1.5,
+  },
+  image: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    borderRadius: 1,
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8 )',
+    padding: 5,
+    paddingBottom: 0,
+    maxWidth: width / 3,
+    borderRadius: 2,
+  },
+  title: {
+    fontFamily: fonts.bold,
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
