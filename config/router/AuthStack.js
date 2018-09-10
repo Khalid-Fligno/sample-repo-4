71cfb2f6b4eba66dc914c5ea8a1b242d8dfaac32
Caleb Harnell <calebharnell@hotmail.com>
2018-09-11 09:15:17 +1000

    Add email verification screen to router

--- config/router/AuthStack.js
+++ config/router/AuthStack.js
@@ -2,12 +2,14 @@ import { createStackNavigator } from 'react-navigation';
 import LandingScreen from '../../src/screens/AuthStack/LandingScreen';
 import LoginScreen from '../../src/screens/AuthStack/LoginScreen';
 import SignupScreen from '../../src/screens/AuthStack/SignupScreen';
+import EmailVerificationScreen from '../../src/screens/AuthStack/EmailVerificationScreen';
 
 const AuthStack = createStackNavigator(
   {
     Landing: LandingScreen,
     Signup: SignupScreen,
     Login: LoginScreen,
+    EmailVerification: EmailVerificationScreen,
   },
   {
     initialRouteName: 'Landing',
