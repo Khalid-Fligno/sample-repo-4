1a9c0a0c560d955bf36d9b1e4883ed2e80f7adec
Caleb Harnell <calebharnell@hotmail.com>
2018-09-14 09:24:33 +1000

    Update picker options

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,56 @@
+export const uomMap = {
+  metric: 'Metric (cm, kg)',
+  imperial: 'Imperial (inch, lb)',
+};
+
+export const weightOptions = [
+  { value: 30, label: '30' },
+];
+
+const populateWeightOptions = () => {
+  for (let i = 31; i <= 120; i += 1) {
+    weightOptions.push({
+      value: i,
+      label: `${i}`,
+    });
+  }
+};
+populateWeightOptions();
+
+export const waistOptions = [
+  { value: 20, label: '20' },
+];
+
+const populateWaistOptions = () => {
+  for (let i = 21; i <= 120; i += 1) {
+    waistOptions.push({
+      value: i,
+      label: `${i}`,
+    });
+  }
+};
+populateWaistOptions();
+
+export const hipOptions = [
+  { value: 20, label: '20' },
+];
+
+const populateHipOptions = () => {
+  for (let i = 21; i <= 120; i += 1) {
+    hipOptions.push({
+      value: i,
+      label: `${i}`,
+    });
+  }
+};
+populateHipOptions();
+
 export const burpeeOptions = [
-  { value: null, label: 'Select your completed burpee count' },
+  { value: 0, label: '0' },
 ];
 
 const populateBurpeeOptions = () => {
-  for (let i = 0; i < 50; i += 1) {
+  for (let i = 1; i <= 50; i += 1) {
     burpeeOptions.push({
       value: i,
       label: `${i}`,
@@ -13,7 +60,7 @@ const populateBurpeeOptions = () => {
 populateBurpeeOptions();
 
 export const findFitnessLevel = (burpeeCount) => {
-  if (burpeeCount < 12) {
+  if (burpeeCount <= 12) {
     return 1;
   } else if (burpeeCount > 16) {
     return 3;
@@ -31,45 +78,3 @@ export const findReps = (fitnessLevel) => {
   }
   return 10;
 };
-
-export const weightOptions = [
-  { value: 30, label: '30' },
-];
-
-const populateWeightOptions = () => {
-  for (let i = 31; i < 120; i += 1) {
-    weightOptions.push({
-      value: i,
-      label: `${i}`,
-    });
-  }
-};
-populateWeightOptions();
-
-export const waistOptions = [
-  { value: 20, label: '20' },
-];
-
-const populateWaistOptions = () => {
-  for (let i = 21; i < 120; i += 1) {
-    waistOptions.push({
-      value: i,
-      label: `${i}`,
-    });
-  }
-};
-populateWaistOptions();
-
-export const hipOptions = [
-  { value: 20, label: '20' },
-];
-
-const populateHipOptions = () => {
-  for (let i = 21; i < 120; i += 1) {
-    hipOptions.push({
-      value: i,
-      label: `${i}`,
-    });
-  }
-};
-populateHipOptions();
