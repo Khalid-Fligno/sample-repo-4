cde7798b03119553401f3f7dcd54724f17d3f039
Caleb Harnell <calebharnell@hotmail.com>
2018-09-14 16:25:49 +1000

    Update navigation for retest

--- src/screens/AppStack/Progress/ProgressHomeScreen.js
+++ src/screens/AppStack/Progress/ProgressHomeScreen.js
@@ -221,7 +221,7 @@ export default class ProgressHomeScreen extends React.PureComponent {
           </View>
           <CustomButton
             title="RETEST YOUR PROGRESS"
-            onPress={() => this.props.navigation.navigate('Onboarding2', { isInitial: false })}
+            onPress={() => this.props.navigation.navigate('Progress1', { isInitial: false })}
           />
         </ScrollView>
       </View>
