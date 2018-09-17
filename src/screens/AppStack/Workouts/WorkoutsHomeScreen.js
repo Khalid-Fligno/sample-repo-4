e0bbb34090ae50bf78de5f8fa448875874d9fbff
Caleb Harnell <calebharnell@hotmail.com>
2018-09-17 16:37:03 +1000

    New workouts home screen (In Progress)

--- src/screens/AppStack/Workouts/WorkoutsHomeScreen.js
+++ src/screens/AppStack/Workouts/WorkoutsHomeScreen.js
@@ -1,19 +1,211 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Tile from '../../../components/Tile';
+import { StyleSheet, View, Text, Dimensions, ImageBackground } from 'react-native';
+import { FileSystem } from 'expo';
+import Carousel from 'react-native-snap-carousel';
+import FadeInView from 'react-native-fade-in-view';
+import CustomButton from '../../../components/CustomButton';
+import Loader from '../../../components/Loader';
+// import Tile from '../../../components/Tile';
+import { db } from '../../../../config/firebase';
 import colors from '../../../styles/colors';
+import fonts from '../../../styles/fonts';
+
+const { width } = Dimensions.get('window');
+
+const workoutTypes = [
+  { displayName: 'RESISTANCE' },
+  { displayName: 'HIIT' },
+];
+
+const workoutLocations = [
+  { displayName: 'GYM', image: require('../../../../assets/images/workouts-gym.jpg') },
+  { displayName: 'HOME', image: require('../../../../assets/images/workouts-home.jpg') },
+  { displayName: 'PARK', image: require('../../../../assets/images/workouts-park.jpg') },
+];
+
+const resistanceWorkouts = [
+  { displayName: 'FULL BODY', image: require('../../../../assets/images/workouts-full.jpg') },
+  { displayName: 'UPPER BODY', image: require('../../../../assets/images/workouts-upper.jpg') },
+  { displayName: 'LOWER BODY', image: require('../../../../assets/images/workouts-lower.jpg') },
+  { displayName: 'CORE', image: require('../../../../assets/images/workouts-core.jpg') },
+];
+
+const hiitWorkouts = [
+  { displayName: 'JOGGING' },
+  { displayName: 'CYCLING' },
+  { displayName: 'ROWING' },
+  { displayName: 'SKIPPING' },
+];
+
+const workoutFocusMap = {
+  0: 'fullBody',
+  1: 'upperBody',
+  2: 'lowerBody',
+  3: 'core',
+};
+
+const workoutLocationMap = {
+  0: 'gym',
+  1: 'home',
+  2: 'park',
+};
+
+const hiitTypeMap = {
+  0: 'jogging',
+  1: 'cycling',
+  2: 'rowing',
+  3: 'skipping',
+};
 
 export default class WorkoutsHomeScreen extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
+      selectedWorkoutTypeIndex: 0,
+      selectedWorkoutLocationIndex: 0,
+      selectedHiitWorkoutIndex: 0,
+      selectedResistanceFocusIndex: 0,
     };
   }
+  handleWorkoutSelected = () => {
+    const {
+      selectedWorkoutTypeIndex,
+      selectedWorkoutLocationIndex,
+      selectedHiitWorkoutIndex,
+      selectedResistanceFocusIndex,
+    } = this.state;
+    if (selectedWorkoutTypeIndex === 0) {
+      const workoutType = workoutFocusMap[selectedResistanceFocusIndex];
+      const workoutLocation = workoutLocationMap[selectedWorkoutLocationIndex];
+      this.props.navigation.navigate('WorkoutsSelection', {
+        workoutType,
+        workoutLocation,
+      });
+    } else {
+      console.log(selectedWorkoutTypeIndex);
+      console.log(selectedHiitWorkoutIndex);
+    }
+  }
+  handleHiitWorkoutSelected = async (selectedHiitWorkoutIndex) => {
+    this.setState({ loading: true });
+    const type = hiitTypeMap[selectedHiitWorkoutIndex];
+    try {
+      this.unsubscribe = await db.collection('workouts')
+        .where(type, '==', true)
+        .get()
+        .then(async (querySnapshot) => {
+          let workout;
+          await querySnapshot.forEach(async (doc) => {
+            workout = await doc.data();
+          });
+          const { exercises } = workout;
+          await Promise.all(exercises.map(async (exercise, index) => {
+            await FileSystem.downloadAsync(
+              exercise.videoURL,
+              `${FileSystem.cacheDirectory}exercise-${index + 1}.mp4`,
+            );
+          }));
+          this.setState({ loading: false });
+          this.props.navigation.navigate('HiitWorkoutInfo', { workout });
+        });
+    } catch (err) {
+      this.setState({ loading: false });
+    }
+  }
+
+  renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.slide}>
+        <ImageBackground
+          source={item.image || require('../../../../assets/images/workouts-upper.jpg')}
+          style={styles.image}
+        >
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>
+              {item.displayName}
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
   render() {
-    const { navigate } = this.props.navigation;
+    const {
+      loading,
+      selectedWorkoutTypeIndex,
+      selectedWorkoutLocationIndex,
+      selectedHiitWorkoutIndex,
+      selectedResistanceFocusIndex,
+    } = this.state;
     return (
       <View style={styles.container}>
-        <Tile
+        <View style={styles.carouselsContainer}>
+          <View style={{ flex: 1 }}>
+            <Carousel
+              ref={(c) => { this.carousel = c; }}
+              data={workoutTypes}
+              renderItem={this.renderItem}
+              sliderWidth={width}
+              itemWidth={width * 0.85}
+              onSnapToItem={(slideIndex) => this.setState({ selectedWorkoutTypeIndex: slideIndex })}
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            {
+              selectedWorkoutTypeIndex === 0 && (
+                <FadeInView duration={1000} style={{ flex: 1 }}>
+                  <Carousel
+                    ref={(c) => { this.carousel = c; }}
+                    data={resistanceWorkouts}
+                    renderItem={this.renderItem}
+                    sliderWidth={width}
+                    itemWidth={width * 0.85}
+                    onSnapToItem={(slideIndex) => this.setState({ selectedResistanceFocusIndex: slideIndex })}
+                  />
+                </FadeInView>
+              )
+            }
+            {
+              selectedWorkoutTypeIndex === 1 && (
+                <FadeInView duration={1000} style={{ flex: 1 }}>
+                  <Carousel
+                    ref={(c) => { this.carousel = c; }}
+                    data={hiitWorkouts}
+                    renderItem={this.renderItem}
+                    sliderWidth={width}
+                    itemWidth={width * 0.85}
+                    onSnapToItem={(slideIndex) => this.setState({ selectedHiitWorkoutIndex: slideIndex })}
+                  />
+                </FadeInView>
+              )
+            }
+          </View>
+          <View style={{ flex: 1 }}>
+            {
+              selectedWorkoutTypeIndex === 0 && (
+                <FadeInView duration={1500} style={{ flex: 1 }}>
+                  <Carousel
+                    ref={(c) => { this.carousel = c; }}
+                    data={workoutLocations}
+                    renderItem={this.renderItem}
+                    sliderWidth={width}
+                    itemWidth={width * 0.85}
+                    onSnapToItem={(slideIndex) => this.setState({ selectedWorkoutLocationIndex: slideIndex })}
+                  />
+                </FadeInView>
+              )
+            }
+          </View>
+        </View>
+        <View style={{ paddingBottom: 10 }}>
+          <CustomButton
+            title={selectedWorkoutTypeIndex === 0 ? 'SHOW WORKOUTS' : 'GO TO WORKOUT'}
+            onPress={selectedWorkoutTypeIndex === 0 ? () => this.handleWorkoutSelected() : () => this.handleHiitWorkoutSelected(selectedHiitWorkoutIndex)}
+            primary
+          />
+        </View>
+        {/* <Tile
           title1="FULL BODY"
           image={require('../../../../assets/images/workouts-full.jpg')}
           onPress={() => navigate('WorkoutsLocation', { workoutType: 'fullBody' })}
@@ -32,7 +224,10 @@ export default class WorkoutsHomeScreen extends React.PureComponent {
           title1="CORE"
           image={require('../../../../assets/images/workouts-core.jpg')}
           onPress={() => navigate('WorkoutsLocation', { workoutType: 'core' })}
-        />
+        /> */}
+        {
+          loading && <Loader color={colors.coral.standard} loading={loading} />
+        }
       </View>
     );
   }
@@ -43,8 +238,43 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.offWhite,
     alignItems: 'center',
+  },
+  carouselsContainer: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    shadowColor: colors.charcoal.standard,
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+  },
+  slide: {
+    flex: 1,
+    marginTop: 12.5,
+    marginBottom: 12.5,
+    backgroundColor: 'green',
+    borderRadius: 2,
+    overflow: 'hidden',
+  },
+  image: {
+    flex: 1,
+    alignItems: 'flex-start',
     justifyContent: 'center',
-    paddingTop: 5,
-    paddingBottom: 5,
+    paddingLeft: 25,
+    overflow: 'hidden',
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.65 )',
+    paddingTop: 8,
+    paddingRight: 12,
+    paddingBottom: 3,
+    paddingLeft: 12,
+    borderRadius: 2,
+  },
+  title: {
+    fontFamily: fonts.bold,
+    fontSize: 24,
+    color: colors.black,
+    textAlign: 'center',
   },
 });
