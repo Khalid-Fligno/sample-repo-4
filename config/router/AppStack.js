59b7710b51ca0e8369e70a19488fc6f59b7b5809
Caleb Harnell <calebharnell@hotmail.com>
2018-09-17 16:27:08 +1000

    Add separate stack for hiit exercises

--- config/router/AppStack.js
+++ config/router/AppStack.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from 'react-navigation';
 import TabStack from './TabStack';
 import ExerciseStack from './ExerciseStack';
+import HiitExerciseStack from './HiitExerciseStack';
 import ProfileStack from './ProfileStack';
 import { fadeSpec, fade } from './utils';
 
@@ -9,6 +10,7 @@ const AppStack = createStackNavigator(
     Tabs: { screen: TabStack },
     Profile: { screen: ProfileStack },
     Exercise: { screen: ExerciseStack },
+    HiitExercise: { screen: HiitExerciseStack },
   },
   {
     initialRouteName: 'Tabs',
