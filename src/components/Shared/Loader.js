49eff19c49cfb31be68a0d1ad500c37a8bf21b29
Caleb Harnell <calebharnell@hotmail.com>
2018-11-28 16:08:38 +1000

    Optional text for Loader

--- src/components/Shared/Loader.js
+++ src/components/Shared/Loader.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import { DotIndicator } from 'react-native-indicators';
 import PropTypes from 'prop-types';
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 const Loader = ({
   loading,
   color,
   overlayColor,
+  text,
 }) => {
   if (loading) {
     return (
@@ -19,11 +21,22 @@ const Loader = ({
           size="small"
           overlayColor={overlayColor}
         >
-          <DotIndicator
-            color={color}
-            count={3}
-            size={10}
-          />
+          <View style={styles.contentContainer}>
+            <View style={styles.dotIndicatorContainer}>
+              <DotIndicator
+                color={color}
+                count={3}
+                size={10}
+              />
+            </View>
+            {
+              text && (
+                <Text style={styles.loaderText}>
+                  {text}
+                </Text>
+              )
+            }
+          </View>
         </Spinner>
       </View>
     );
@@ -35,11 +48,13 @@ Loader.propTypes = {
   loading: PropTypes.bool.isRequired,
   color: PropTypes.string,
   overlayColor: PropTypes.string,
+  text: PropTypes.string,
 };
 
 Loader.defaultProps = {
   overlayColor: 'rgba(0, 0, 0, 0.6)',
   color: colors.white,
+  text: undefined,
 };
 
 const styles = StyleSheet.create({
@@ -47,6 +62,20 @@ const styles = StyleSheet.create({
     position: 'absolute',
     flex: 1,
     backgroundColor: colors.white,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dotIndicatorContainer: {
+    height: 30,
+  },
+  loaderText: {
+    color: colors.white,
+    fontSize: 16,
+    fontFamily: fonts.bold,
+    justifyContent: 'flex-start',
   },
 });
 
