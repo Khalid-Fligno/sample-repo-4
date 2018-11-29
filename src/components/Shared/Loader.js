c752eafd6109ea120d687078f6ce857615551590
Caleb Harnell <calebharnell@hotmail.com>
2018-11-29 13:05:59 +1000

    Add optional loader text

--- src/components/Shared/Loader.js
+++ src/components/Shared/Loader.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   dotIndicatorContainer: {
-    height: 30,
+    height: 50,
   },
   loaderText: {
     color: colors.white,
