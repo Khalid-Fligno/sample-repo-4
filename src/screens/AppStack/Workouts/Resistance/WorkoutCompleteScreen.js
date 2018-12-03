0d1edf59bb19060fd33dfca31a401317e4e25e31
Caleb Harnell <calebharnell@hotmail.com>
2018-12-03 10:36:21 +1000

    Add text, icon and button to workout complete screens

--- src/screens/AppStack/Workouts/Resistance/WorkoutCompleteScreen.js
+++ src/screens/AppStack/Workouts/Resistance/WorkoutCompleteScreen.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { StyleSheet, View, Text, AsyncStorage, Alert } from 'react-native';
+import { StyleSheet, View, Text, AsyncStorage, Alert, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 import { FileSystem } from 'expo';
+import { PieChart } from 'react-native-svg-charts';
 import Loader from '../../../../components/Shared/Loader';
+import Icon from '../../../../components/Shared/Icon';
+import CustomButton from '../../../../components/Shared/CustomButton';
 import { db } from '../../../../../config/firebase';
+import fonts from '../../../../styles/fonts';
 import colors from '../../../../styles/colors';
+
+const { width } = Dimensions.get('window');
 
 const updateTallies = (obj, resistanceCategoryId, newTally) => {
   return Object.assign({}, obj, { [resistanceCategoryId]: newTally });
 };
+
+const pieDataComplete = [100, 0]
+  .map((value, index) => ({
+    value,
+    svg: {
+      fill: colors.coral.standard,
+    },
+    key: `pie-${index}`,
+  }));
 
 export default class WorkoutCompleteScreen extends React.PureComponent {
   constructor(props) {
@@ -56,21 +72,52 @@ export default class WorkoutCompleteScreen extends React.PureComponent {
   }
   render() {
     const { loading, resistanceCategoryId } = this.state;
-    return (
-      <View style={styles.container}>
-        <Text>
-          Workout Complete
-        </Text>
-        <Text
-          onPress={() => this.completeWorkout(resistanceCategoryId)}
-        >
-          Done
-        </Text>
-        <Loader
-          color={colors.coral.standard}
-          loading={loading}
-        />
+    const completePieChart = (
+      <PieChart
+        style={styles.pieChart}
+        data={pieDataComplete}
+        innerRadius="80%"
+      />
+    );
+    const tickIcon = (
+      <View style={styles.invisibleView}>
+        <View style={styles.tickContainer}>
+          <Icon
+            name="tick-heavy"
+            color={colors.charcoal.dark}
+            size={60}
+          />
+        </View>
       </View>
+    );
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.flexContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.headerText}>
+              Workout Complete
+            </Text>
+            <Text style={styles.bodyText}>
+              Congratulations!  Press the button at the bottom of your screen to complete your workout.
+            </Text>
+          </View>
+          <View style={styles.iconContainer}>
+            {completePieChart}
+            {tickIcon}
+          </View>
+          <View style={styles.buttonContainer}>
+            <CustomButton
+              title="COMPLETE"
+              onPress={() => this.completeWorkout(resistanceCategoryId)}
+              primary
+            />
+          </View>
+          <Loader
+            color={colors.coral.standard}
+            loading={loading}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -78,8 +125,50 @@ export default class WorkoutCompleteScreen extends React.PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.black,
+  },
+  flexContainer: {
+    flex: 1,
     backgroundColor: colors.white,
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    flex: 1,
+    width,
+    padding: 10,
+    paddingTop: 15,
+  },
+  headerText: {
+    fontFamily: fonts.bold,
+    fontSize: 28,
+    color: colors.charcoal.light,
+    marginBottom: 10,
+  },
+  bodyText: {
+    fontFamily: fonts.standard,
+    fontSize: 14,
+    color: colors.charcoal.light,
+    marginLeft: 3,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  pieChart: {
+    height: 120,
+    width: 120,
+  },
+  invisibleView: {
+    height: 0,
+  },
+  tickContainer: {
+    marginTop: -90,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 10,
   },
 });
