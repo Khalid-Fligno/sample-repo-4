ca0cef7aeb163678aec715b8618e1ca1613adb8b
Caleb Harnell <calebharnell@hotmail.com>
2018-12-17 10:09:30 +1000

    Add analytics to homescreen links

--- src/screens/AppStack/Home/HomeScreen.js
+++ src/screens/AppStack/Home/HomeScreen.js
@@ -4,7 +4,7 @@ import {
   View,
   Linking,
 } from 'react-native';
-import { Haptic } from 'expo';
+import { Haptic, Segment } from 'expo';
 import DoubleNewsFeedTile from '../../../components/Home/DoubleNewsFeedTile';
 import NewsFeedTile from '../../../components/Home/NewsFeedTile';
 import colors from '../../../styles/colors';
@@ -15,9 +15,10 @@ export default class HomeScreen extends React.PureComponent {
     this.state = {
     };
   }
-  openLink = (url) => {
+  openLink = (url, analyticsKey) => {
     Haptic.impact(Haptic.ImpactFeedbackStyle.Medium);
     Linking.openURL(url);
+    Segment.track(`Link opened: ${analyticsKey}`);
   }
   render() {
     return (
@@ -29,18 +30,18 @@ export default class HomeScreen extends React.PureComponent {
           titleLeft2="SLEEP"
           titleRight1="BLOG POST:"
           titleRight2="MINDSET"
-          onPressLeft={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/27/hitting-the-hay-the-right-way')}
-          onPressRight={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/3/get-your-head-in-the-weight-loss-game')}
+          onPressLeft={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/27/hitting-the-hay-the-right-way', 'Sleep Blog')}
+          onPressRight={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/3/get-your-head-in-the-weight-loss-game', 'Mindset Blog')}
         />
         <NewsFeedTile
           image={require('../../../../assets/images/shop-bundles.jpg')}
           title="GRAB YOUR FITAZFK WORKOUT EQUIPMENT"
-          onPress={() => this.openLink('https://fitazfk.com/collections/equipment')}
+          onPress={() => this.openLink('https://fitazfk.com/collections/equipment', 'Online Store')}
         />
         <NewsFeedTile
           image={require('../../../../assets/images/fitazfk-army.jpg')}
           title="JOIN OUR FACEBOOK GROUP"
-          onPress={() => this.openLink('https://www.facebook.com/groups/180007149128432/?source_id=204363259589572')}
+          onPress={() => this.openLink('https://www.facebook.com/groups/180007149128432/?source_id=204363259589572', 'Facebook Group')}
         />
       </View>
     );
