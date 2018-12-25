cef889a5064fbf244def1d89b320351622874b0b
Caleb Harnell <calebharnell@hotmail.com>
2018-12-25 16:59:46 +1000

    Turn sentry off in development

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Sentry from 'sentry-expo';
 import SwitchNavigator from './config/router/index';
 
-// Remove this once Sentry is correctly setup.
-Sentry.enableInExpoDevelopment = true;
-
 Sentry.config('https://ad25f20f55644584bd7ef1ffd7dfe1f1@sentry.io/1342308').install();
 
 export default class App extends React.PureComponent {
