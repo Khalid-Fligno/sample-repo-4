8ab199586722cdfbffd3fc380331acdef0f9f2ed
Caleb Harnell <calebharnell@hotmail.com>
2019-01-10 22:13:51 +1000

    Create exercise info button component

---
+++ src/components/Workouts/ExerciseInfoButton.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import Icon from '../Shared/Icon';
+import colors from '../../styles/colors';
+
+const { width } = Dimensions.get('window');
+
+const ExerciseInfoButton = ({ onPress }) => (
+  <View style={styles.invisibleView}>
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.helpButton}
+    >
+      <Icon
+        name="question-speech-bubble"
+        size={35}
+        color={colors.white}
+        style={styles.icon}
+      />
+    </TouchableOpacity>
+  </View>
+);
+
+ExerciseInfoButton.propTypes = {
+  onPress: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+  invisibleView: {
+    height: 0,
+    width,
+    alignItems: 'flex-end',
+  },
+  helpButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: 52,
+    position: 'absolute',
+    marginTop: -50,
+    backgroundColor: colors.transparentBlackLight,
+    borderTopLeftRadius: 4,
+  },
+  icon: {
+    marginTop: 2,
+    marginLeft: 2,
+  },
+});
+
+export default ExerciseInfoButton;
