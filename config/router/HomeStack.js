8dcfcd82ec41da9d7a891f3e6a4fdd2c902231ce
Caleb Harnell <calebharnell@hotmail.com>
2019-01-14 16:21:23 +1000

    Add blog screen to home stack

--- config/router/HomeStack.js
+++ config/router/HomeStack.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import HomeScreen from '../../src/screens/AppStack/Home/HomeScreen';
+import BlogScreen from '../../src/screens/AppStack/Home/BlogScreen';
 import Header from '../../src/components/Shared/Header';
 
 const HomeStack = createStackNavigator(
   {
     HomeHome: HomeScreen,
+    HomeBlog: BlogScreen,
   },
   {
     initialRouteName: 'HomeHome',
@@ -14,8 +16,10 @@ const HomeStack = createStackNavigator(
         const { routeName } = navigation.state;
         return (
           <Header
+            stack="home"
             navigation={navigation}
             withProfileButton={routeName === 'HomeHome'}
+            withBackButton={routeName === 'HomeBlog'}
           />
         );
       },
