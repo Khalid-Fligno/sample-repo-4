55a9f1cef65aac900af164488526c3d8a93f639e
Caleb Harnell <calebharnell@hotmail.com>
2019-01-14 16:23:13 +1000

    Update home screen

--- src/screens/AppStack/Home/HomeScreen.js
+++ src/screens/AppStack/Home/HomeScreen.js
@@ -4,16 +4,12 @@ import {
   View,
   Linking,
   ScrollView,
-  Text,
-  AsyncStorage,
 } from 'react-native';
 import { Haptic, Segment } from 'expo';
-import DoubleNewsFeedTile from '../../../components/Home/DoubleNewsFeedTile';
 import NewsFeedTile from '../../../components/Home/NewsFeedTile';
+import DoubleNewsFeedTile from '../../../components/Home/DoubleNewsFeedTile';
+import TripleNewsFeedTile from '../../../components/Home/TripleNewsFeedTile';
 import Loader from '../../../components/Shared/Loader';
-import ProgressBar from '../../../components/Home/ProgressBar';
-import { db } from '../../../../config/firebase';
-import fonts from '../../../styles/fonts';
 import colors from '../../../styles/colors';
 
 export default class HomeScreen extends React.PureComponent {
@@ -21,26 +17,7 @@ export default class HomeScreen extends React.PureComponent {
     super(props);
     this.state = {
       loading: false,
-      profile: undefined,
     };
-  }
-  componentDidMount = () => {
-    this.fetchProfile();
-  }
-  componentWillUnmount = () => {
-    this.unsubscribe();
-  }
-  fetchProfile = async () => {
-    this.setState({ loading: true });
-    const uid = await AsyncStorage.getItem('uid');
-    this.unsubscribe = await db.collection('users').doc(uid)
-      .onSnapshot(async (doc) => {
-        if (doc.exists) {
-          this.setState({ profile: await doc.data(), loading: false });
-        } else {
-          this.setState({ loading: false });
-        }
-      });
   }
   openLink = (url, analyticsKey) => {
     Haptic.impact(Haptic.ImpactFeedbackStyle.Medium);
@@ -48,58 +25,44 @@ export default class HomeScreen extends React.PureComponent {
     Segment.track(`Link opened: ${analyticsKey}`);
   }
   render() {
-    const { loading, profile } = this.state;
+    const { loading } = this.state;
     return (
       <View style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollView}>
-          <View style={styles.homeInfoContainer}>
-            <Text style={styles.welcomeText}>
-              { profile && `Welcome, ${profile.firstName}`}!
-            </Text>
-            <Text style={styles.bodyText}>
-              Here is your progress for the week
-            </Text>
-            {
-              profile && (
-                <ProgressBar
-                  progressBarType="resistance"
-                  completedWorkouts={profile.weeklyTargets.resistanceWeeklyComplete}
-                />
-              )
-            }
-            {
-              profile && (
-                <ProgressBar
-                  progressBarType="hiit"
-                  completedWorkouts={profile.weeklyTargets.hiitWeeklyComplete}
-                />
-              )
-            }
-          </View>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollView}
+        >
           <DoubleNewsFeedTile
-            imageLeft={require('../../../../assets/images/fitazfk-blog-sleep.jpg')}
-            imageRight={require('../../../../assets/images/fitazfk-blog-mindset.jpg')}
-            titleLeft1="BLOG POST:"
-            titleLeft2="SLEEP"
-            titleRight1="BLOG POST:"
-            titleRight2="MINDSET"
-            onPressLeft={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/27/hitting-the-hay-the-right-way', 'Sleep Blog')}
-            onPressRight={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/3/get-your-head-in-the-weight-loss-game', 'Mindset Blog')}
+            imageLeft={require('../../../../assets/images/homeScreenTiles/home-screen-nutrition.jpg')}
+            imageRight={require('../../../../assets/images/homeScreenTiles/home-screen-workouts.jpg')}
+            titleLeft1="NUTRITION"
+            titleRight1="WORKOUTS"
+            onPressLeft={() => this.props.navigation.navigate('Nutrition')}
+            onPressRight={() => this.props.navigation.navigate('Workouts')}
           />
           <DoubleNewsFeedTile
-            imageLeft={require('../../../../assets/images/fitazfk-blog-sleep.jpg')}
-            imageRight={require('../../../../assets/images/fitazfk-blog-mindset.jpg')}
-            titleLeft1="BLOG POST:"
-            titleLeft2="SLEEP"
-            titleRight1="BLOG POST:"
-            titleRight2="MINDSET"
-            onPressLeft={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/27/hitting-the-hay-the-right-way', 'Sleep Blog')}
-            onPressRight={() => this.openLink('https://www.fitazfkblog.com/home/2018/9/3/get-your-head-in-the-weight-loss-game', 'Mindset Blog')}
+            imageLeft={require('../../../../assets/images/homeScreenTiles/home-screen-calendar.jpg')}
+            imageRight={require('../../../../assets/images/homeScreenTiles/home-screen-blog.jpg')}
+            titleLeft1="CALENDAR"
+            titleRight1="BLOG"
+            onPressLeft={() => this.props.navigation.navigate('Calendar')}
+            onPressRight={() => this.props.navigation.navigate('HomeBlog')}
           />
           <NewsFeedTile
             image={require('../../../../assets/images/shop-bundles.jpg')}
             title="GRAB YOUR FITAZFK WORKOUT EQUIPMENT"
             onPress={() => this.openLink('https://fitazfk.com/collections/equipment', 'Online Store')}
+          />
+          <TripleNewsFeedTile
+            imageLeft={require('../../../../assets/images/hiit-rest-placeholder.jpg')}
+            imageCenter={require('../../../../assets/images/homeScreenTiles/home-screen-profile.jpg')}
+            imageRight={require('../../../../assets/images/workouts-hiit-rowing.jpg')}
+            titleLeft="FAQ"
+            titleCenter="PROFILE"
+            titleRight="PROGRESS"
+            onPressLeft={() => this.openLink('https://fitazfk.zendesk.com/hc/en-us', 'FAQ')}
+            onPressCenter={() => this.props.navigation.navigate('ProfileHome')}
+            onPressRight={() => this.props.navigation.navigate('Progress')}
           />
           <NewsFeedTile
             image={require('../../../../assets/images/fitazfk-army.jpg')}
@@ -123,25 +86,5 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     padding: 5,
-  },
-  homeInfoContainer: {
-    margin: 5,
-    backgroundColor: colors.white,
-    borderRadius: 2,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: colors.grey.light,
-  },
-  welcomeText: {
-    fontFamily: fonts.bold,
-    fontSize: 22,
-    color: colors.charcoal.light,
-    marginBottom: 5,
-  },
-  bodyText: {
-    fontFamily: fonts.standard,
-    fontSize: 14,
-    color: colors.charcoal.light,
-    marginBottom: 5,
   },
 });
