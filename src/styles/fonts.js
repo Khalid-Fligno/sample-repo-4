5a4cc4bb67921d8b66fd410e9c285a7dc68d5418
Caleb Harnell <calebharnell@hotmail.com>
2019-01-14 14:16:47 +1000

    Remove redundant assets

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,12 +1,8 @@
 const fonts = {
   bold: 'GothamBold',
   boldNarrow: 'GothamNarrowBold',
-  boldItalic: 'GothamBoldItalic',
-  medium: 'GothamMedium',
   standard: 'GothamBook',
   standardNarrow: 'GothamNarrowBook',
-  light: 'GothamNarrowLight',
-  knucklebones: 'Knucklebones',
 };
 
 export default fonts;
