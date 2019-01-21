ffc56562e654752676fb9263e4b7b163cd8557f4
Caleb Harnell <calebharnell@hotmail.com>
2019-01-21 22:57:25 +1000

    Correct font size subscription tile

--- src/components/Onboarding/SubscriptionTile.js
+++ src/components/Onboarding/SubscriptionTile.js
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
   },
   subscriptionTileHeader: {
     fontFamily: fonts.bold,
-    fontSize: 12,
+    fontSize: 14,
     color: colors.white,
     marginTop: 5,
     marginBottom: 3,
