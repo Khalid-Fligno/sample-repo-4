e317ee979485709a1111e366da1b67c858535305
Caleb Harnell <calebharnell@hotmail.com>
2019-06-03 11:12:03 +1000

    Create circuit countdown screen

---
+++ src/screens/AppStack/Workouts/HiitCircuit/HiitCircuitCountdownScreen.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { FileSystem } from 'expo';
+import { SafeAreaView } from 'react-navigation';
+import CountdownTimer from '../../../../components/Workouts/CountdownTimer';
+import colors from '../../../../styles/colors';
+import fonts from '../../../../styles/fonts';
+
+export default class HiitCircuitCountdownScreen extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exerciseList: this.props.navigation.getParam('exerciseList', null),
+      fitnessLevel: this.props.navigation.getParam('fitnessLevel', null),
+      countdownDuration: 5,
+      countdownActive: false,
+    };
+  }
+  componentDidMount() {
+    this.startCountdown();
+    this.checkVideoCache();
+  }
+  startCountdown = () => {
+    this.setState({
+      countdownActive: true,
+    });
+  }
+  checkVideoCache = async () => {
+    const { exerciseList } = this.state;
+    const video1 = await FileSystem.getInfoAsync(`${FileSystem.cacheDirectory}exercise-hiit-circuit-1.mp4`);
+    if (!video1.exists) {
+      Promise.all(exerciseList.map(async (exercise, index) => {
+        FileSystem.downloadAsync(
+          exercise.videoURL,
+          `${FileSystem.cacheDirectory}exercise-hiit-circuit-${index + 1}.mp4`,
+        );
+      }));
+    }
+  }
+  finishCountdown = (exerciseList, fitnessLevel) => {
+    this.setState({ countdownActive: false });
+    this.props.navigation.navigate('HiitCircuitExercise1', {
+      exerciseList,
+      fitnessLevel,
+    });
+  }
+  render() {
+    const {
+      exerciseList,
+      countdownDuration,
+      countdownActive,
+      fitnessLevel,
+    } = this.state;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.countdownContainer}>
+          <CountdownTimer
+            totalDuration={countdownDuration}
+            start={countdownActive}
+            handleFinish={() => this.finishCountdown(exerciseList, fitnessLevel)}
+          />
+          <Text style={styles.countdownText}>
+            GET READY!
+          </Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  countdownContainer: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  countdownText: {
+    fontFamily: fonts.bold,
+    fontSize: 28,
+    color: colors.charcoal.standard,
+  },
+});
