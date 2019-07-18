041a33e59d0e474401b491aaee6609b18c1c7827
Caleb Harnell <calebharnell@hotmail.com>
2019-07-18 10:17:13 +1000

    Initialise OneSignal

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sentry from 'sentry-expo';
 import { Alert, NetInfo, View, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import SwitchNavigator from './config/router/index';
 import colors from './src/styles/colors';
 
@@ -9,6 +10,14 @@ Sentry.config('https://ad25f20f55644584bd7ef1ffd7dfe1f1@sentry.io/1342308').inst
 export default class App extends React.PureComponent {
   constructor(props) {
     super(props);
+    OneSignal.init(
+      '7078b922-5fed-4cc4-9bf4-2bd718e8b548',
+      {
+        kOSSettingsKeyAutoPrompt: true,
+        kOSSettingsKeyInAppLaunchURL: false,
+      },
+    );
+    OneSignal.configure(); // triggers the ids event
     this.state = {
     };
   }
