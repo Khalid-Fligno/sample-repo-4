bf3623cb10856cf0fc94a8a537f05b24729146e6
Caleb Harnell <calebharnell@hotmail.com>
2019-07-31 13:10:39 +1000

    Add navigation logic and url listener to app.js

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Sentry from 'sentry-expo';
-import { Alert, NetInfo, View, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { Alert, NetInfo, View, SafeAreaView, StyleSheet, StatusBar, Linking } from 'react-native';
 import OneSignal from 'react-native-onesignal';
+import { NavigationActions } from 'react-navigation';
 import SwitchNavigator from './config/router/index';
 import colors from './src/styles/colors';
+
+let navigator;
+
+function setTopLevelNavigator(navigatorRef) {
+  navigator = navigatorRef;
+}
+
+function navigate(routeName, params) {
+  navigator.dispatch(NavigationActions.navigate({
+    routeName,
+    params,
+  }));
+}
 
 Sentry.config('https://ad25f20f55644584bd7ef1ffd7dfe1f1@sentry.io/1342308').install();
 
@@ -24,9 +38,16 @@ export default class App extends React.PureComponent {
   }
   componentDidMount = () => {
     NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);
+    Linking.addEventListener('url', this.handleOpenURL);
   }
   componentWillUnmount = () => {
     NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+    Linking.removeEventListener('url', this.handleOpenURL);
+  }
+  handleOpenURL = (event) => {
+    if (event.url === 'fitazfk://special-offer') {
+      navigate('SpecialOffer');
+    }
   }
   handleConnectivityChange = (connectionInfo) => {
     if (connectionInfo.type === 'none') {
@@ -41,7 +62,9 @@ export default class App extends React.PureComponent {
         <SafeAreaView>
           <StatusBar barStyle="light-content" />
         </SafeAreaView>
-        <SwitchNavigator />
+        <SwitchNavigator
+          ref={(navigatorRef) => setTopLevelNavigator(navigatorRef)}
+        />
       </View>
     );
   }
