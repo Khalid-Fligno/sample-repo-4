643880d16784fcd070498159be91a8519d59b462
Caleb Harnell <calebharnell@hotmail.com>
2019-08-21 14:11:14 +1000

    Update special offer screen styling

--- src/screens/AuthStack/SpecialOfferScreen.js
+++ src/screens/AuthStack/SpecialOfferScreen.js
@@ -3,8 +3,10 @@ import {
   StyleSheet,
   View,
   Text,
+  ImageBackground,
+  SafeAreaView,
 } from 'react-native';
-import CustomButton from '../../components/Shared/CustomButton';
+import { Button } from 'react-native-elements';
 import { auth } from '../../../config/firebase';
 import fonts from '../../styles/fonts';
 import colors from '../../styles/colors';
@@ -26,29 +28,37 @@ export default class SpecialOfferScreen extends React.PureComponent {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.contentContainer}>
-          <Text style={styles.headingLarge}>
-            20% off*
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          source={require('../../../assets/images/special-offer-screen-background.jpg')}
+          style={styles.flexContainer}
+        >
+          <View style={styles.contentContainer}>
+            <Text style={styles.headingLarge}>
+              FK YEAH!
+            </Text>
+            <Text style={styles.heading}>
+              HIT THE BUTTON BELOW TO SCORE YOUR EXCLUSIVE
+            </Text>
+            <Text style={styles.headingLarge}>
+              20% OFF!*
+            </Text>
+            <Button
+              title="REDEEM OFFER NOW"
+              onPress={this.continue}
+              activeOpacity={0.7}
+              buttonStyle={styles.button}
+              textStyle={styles.buttonText}
+            />
+            <Text style={styles.bodyBold}>
+              OFFER VALID FOR 24 HOURS ONLY!
+            </Text>
+          </View>
+          <Text style={styles.disclaimer}>
+            *Based on Australian App store prices.  Discount may be higher or lower based on your local App Store territory pricing tiers.
           </Text>
-          <Text style={styles.heading}>
-            full-priced
-          </Text>
-          <Text style={styles.heading}>
-            subscriptions
-          </Text>
-          <Text style={styles.body}>
-            *Based on Australian App store prices.  Discount may be higher or lower based on your App Store territory pricing tiers.
-          </Text>
-        </View>
-        <View style={styles.buttonContainer}>
-          <CustomButton
-            title="REDEEM OFFER"
-            onPress={this.continue}
-            primary
-          />
-        </View>
-      </View>
+        </ImageBackground>
+      </SafeAreaView>
     );
   }
 }
@@ -56,7 +66,10 @@ export default class SpecialOfferScreen extends React.PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.offWhite,
+    backgroundColor: colors.black,
+  },
+  flexContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -67,20 +80,48 @@ const styles = StyleSheet.create({
   },
   headingLarge: {
     fontFamily: fonts.ultraItalic,
-    fontSize: 28,
+    fontSize: 36,
+    color: colors.white,
     marginBottom: 20,
   },
   heading: {
-    fontFamily: fonts.ultraItalic,
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  body: {
     fontFamily: fonts.standard,
-    fontSize: 14,
+    fontSize: 20,
+    color: colors.white,
+    textAlign: 'center',
     padding: 15,
+    paddingBottom: 0,
   },
-  buttonContainer: {
-    padding: 10,
+  bodyBold: {
+    fontFamily: fonts.bold,
+    fontSize: 18,
+    color: colors.white,
+    textAlign: 'center',
+    padding: 30,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 45,
+    width: 200,
+    borderRadius: 4,
+    shadowOpacity: 0.4,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 2,
+    backgroundColor: colors.coral.standard,
+    shadowColor: colors.black,
+  },
+  buttonText: {
+    fontFamily: fonts.bold,
+    fontSize: 14,
+    color: colors.white,
+    marginTop: 3,
+  },
+  disclaimer: {
+    fontFamily: fonts.standard,
+    fontSize: 8,
+    color: colors.white,
+    textAlign: 'center',
+    padding: 15,
   },
 });
