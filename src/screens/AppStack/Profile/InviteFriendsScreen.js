9347368e7aabdc670a5ba566a7fffe31866ad322
Caleb Harnell <calebharnell@hotmail.com>
2019-09-10 14:43:41 +1000

    Update friends invited screen

---
+++ src/screens/AppStack/Profile/InviteFriendsScreen.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  AsyncStorage,
+  Dimensions,
+} from 'react-native';
+import Loader from '../../../components/Shared/Loader';
+import CustomButton from '../../../components/Shared/CustomButton';
+import { db } from '../../../../config/firebase';
+import InviteFriends from '../../../components/Profile/InviteFriends';
+import FreeGiftSection from '../../../components/Profile/FreeGiftSection';
+import fonts from '../../../styles/fonts';
+import colors from '../../../styles/colors';
+
+const { width } = Dimensions.get('window');
+
+export default class InviteFriendsScreen extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactsView: false,
+      friendsInvited: undefined,
+      loading: false,
+    };
+  }
+  componentDidMount() {
+    this.fetchProfile();
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  fetchProfile = async () => {
+    this.setState({ loading: true });
+    const uid = await AsyncStorage.getItem('uid');
+    this.unsubscribe = await db.collection('users').doc(uid)
+      .onSnapshot(async (doc) => {
+        if (doc.exists) {
+          this.setState({ friendsInvited: await doc.data().friendsInvited || undefined, loading: false });
+        } else {
+          this.setState({ loading: false });
+        }
+      });
+  }
+  switchView = () => {
+    this.setState({ contactsView: true });
+  }
+  recordInvites = async (numberOfInvites) => {
+    const uid = await AsyncStorage.getItem('uid');
+    const { friendsInvited } = this.state;
+    const friendsInvitedNew = numberOfInvites + (friendsInvited || 0);
+    const userRef = db.collection('users').doc(uid);
+    await userRef.set({ friendsInvited: friendsInvitedNew }, { merge: true });
+    this.setState({ contactsView: false });
+  }
+  render() {
+    const { friendsInvited, contactsView, loading } = this.state;
+    if (contactsView) {
+      return (
+        <InviteFriends
+          recordInvites={this.recordInvites}
+        />
+      );
+    }
+    return (
+      <SafeAreaView style={styles.safeContainer}>
+        <View style={styles.rewardsContainer}>
+          <ScrollView contentContainerStyle={styles.scrollView}>
+            <Text style={styles.friendsInvitedNumber}>{(friendsInvited && friendsInvited) || 0}</Text>
+            <Text style={styles.friendsInvitedText}>FRIENDS INVITED</Text>
+            <FreeGiftSection
+              isUnlocked={friendsInvited && friendsInvited >= 10}
+              minimumInvites={10}
+              giftName="FitazFK Cookbook"
+              promoCode="INVITECB-30389159"
+            />
+            <FreeGiftSection
+              isUnlocked={friendsInvited && friendsInvited >= 20}
+              minimumInvites={20}
+              giftName="FitazFK Booty Bands"
+              promoCode="INVITEBB-30389159"
+            />
+            <FreeGiftSection
+              isUnlocked={friendsInvited && friendsInvited >= 30}
+              minimumInvites={30}
+              giftName="FitazFK Tank"
+              promoCode="INVITETANK-30389159"
+            />
+          </ScrollView>
+          <View style={styles.buttonContainer}>
+            <CustomButton
+              title="SELECT CONTACTS"
+              onPress={this.switchView}
+              green
+            />
+          </View>
+        </View>
+        <Loader
+          loading={loading}
+          color={colors.charcoal.standard}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  scrollView: {
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  rewardsContainer: {
+    flex: 1,
+    backgroundColor: colors.grey.light,
+    alignItems: 'center',
+  },
+  friendsInvitedNumber: {
+    fontFamily: fonts.bold,
+    fontSize: 42,
+  },
+  friendsInvitedText: {
+    fontFamily: fonts.standardNarrow,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 10,
+    alignItems: 'center',
+    width,
+  },
+});
