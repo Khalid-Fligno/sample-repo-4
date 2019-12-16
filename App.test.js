e2e4c6fc1f41bf6d562967f6e6d036edd50a0f10
Caleb Harnell <calebharnell@hotmail.com>
2019-12-17 09:41:51 +1000

    Snapshot test for App.js

--- App.test.js
+++ App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+beforeAll(() => {
+  return jest.useFakeTimers();
+});
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
