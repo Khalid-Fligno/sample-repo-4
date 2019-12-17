ca83b7660d60ecc0c3566e9166798ffe6ceb0be0
Caleb Harnell <calebharnell@hotmail.com>
2019-12-17 10:05:56 +1000

    Create test file for navigation headers

---
+++ config/router/__tests__/utils.test.js
@@ -0,0 +1,52 @@
+import {
+  findWorkoutsSelectionTitle,
+  findNutritionHeaderTitle,
+} from '../utils';
+
+describe('Find Workouts Selection Title', () => {
+  test('Gym / Full Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'gym', 'fullBody', null)).toBe('GYM / FULL');
+  });
+
+  test('Gym / Upper Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'gym', 'upperBody', null)).toBe('GYM / UPPER');
+  });
+
+  test('Gym / Lower Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'gym', 'lowerBody', null)).toBe('GYM / ABT');
+  });
+
+  test('Home / Full Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'home', 'fullBody', null)).toBe('HOME / FULL');
+  });
+
+  test('Home / Upper Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'home', 'upperBody', null)).toBe('HOME / UPPER');
+  });
+
+  test('Home / Lower Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'home', 'lowerBody', null)).toBe('HOME / ABT');
+  });
+
+  test('Outdoors / Full Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'outdoors', 'fullBody', null)).toBe('OUTDOORS / FULL');
+  });
+
+  test('Outdoors / Upper Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'outdoors', 'upperBody', null)).toBe('OUTDOORS / UPPER');
+  });
+
+  test('Outdoors / Lower Body header displays correctly', () => {
+    expect(findWorkoutsSelectionTitle('WorkoutsSelection', 'outdoors', 'lowerBody', null)).toBe('OUTDOORS / ABT');
+  });
+});
+
+describe('Find Nutrition Header Title', () => {
+  test('Recipe screen header displays correctly', () => {
+    expect(findNutritionHeaderTitle('Recipe')).toBe('RECIPE');
+  });
+
+  test('Method screen header display correctly', () => {
+    expect(findNutritionHeaderTitle('RecipeSteps')).toBe('METHOD');
+  });
+});
