d75d1d5dca1ec33833a8656158f01e1f42440fae
Caleb Harnell <calebharnell@hotmail.com>
2020-01-30 10:37:29 +1000

    Refactor NutritionHomeScreen to functional

--- src/screens/AppStack/Nutrition/NutritionHomeScreen.js
+++ src/screens/AppStack/Nutrition/NutritionHomeScreen.js
@@ -3,38 +3,31 @@ import { StyleSheet, View } from 'react-native';
 import Tile from '../../../components/Shared/Tile';
 import colors from '../../../styles/colors';
 
-export default class NutritionHomeScreen extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Tile
-          title1="BREAKFAST"
-          image={require('../../../../assets/images/nutrition-breakfast.jpg')}
-          onPress={() => this.props.navigation.navigate('RecipeSelection', { meal: 'breakfast' })}
-        />
-        <Tile
-          title1="LUNCH"
-          image={require('../../../../assets/images/nutrition-lunch.jpg')}
-          onPress={() => this.props.navigation.navigate('RecipeSelection', { meal: 'lunch' })}
-        />
-        <Tile
-          title1="DINNER"
-          image={require('../../../../assets/images/nutrition-dinner.jpg')}
-          onPress={() => this.props.navigation.navigate('RecipeSelection', { meal: 'dinner' })}
-        />
-        <Tile
-          title1="SNACK"
-          image={require('../../../../assets/images/nutrition-snack.jpg')}
-          onPress={() => this.props.navigation.navigate('RecipeSelection', { meal: 'snack' })}
-        />
-      </View>
-    );
-  }
+export default function NutritionHomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Tile
+        title1="BREAKFAST"
+        image={require('../../../../assets/images/nutrition-breakfast.jpg')}
+        onPress={() => navigation.navigate('RecipeSelection', { meal: 'breakfast' })}
+      />
+      <Tile
+        title1="LUNCH"
+        image={require('../../../../assets/images/nutrition-lunch.jpg')}
+        onPress={() => navigation.navigate('RecipeSelection', { meal: 'lunch' })}
+      />
+      <Tile
+        title1="DINNER"
+        image={require('../../../../assets/images/nutrition-dinner.jpg')}
+        onPress={() => navigation.navigate('RecipeSelection', { meal: 'dinner' })}
+      />
+      <Tile
+        title1="SNACK"
+        image={require('../../../../assets/images/nutrition-snack.jpg')}
+        onPress={() => navigation.navigate('RecipeSelection', { meal: 'snack' })}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
