b698db89660d4b47dd1616af409b21f0f883817b
nilesh tiwalkar <nileshtiwalkar@rediffmail.com>
2020-10-20 08:27:36 +0530

    changes for shopify node service

--- subscription_service/index.js
+++ subscription_service/index.js
@@ -3,7 +3,16 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const { getSubscriptions } = require('./subscription.js');
 const { getAndroidToken } = require('./android.js');
-const { createShopifyWebhooks, shopifySubscriptionCreated, shopifySubscriptionUpdated, shopifySubscriptionDeleted, getShopifySubscriptions  } = require('./android.js');
+const { 
+    createShopifyWebhooks
+    , shopifyChargeCreated
+    , shopifyChargeUpdated
+    , shopifyChargeDeleted
+    , getShopifyCharges
+    , getShopifyProducts 
+    , getShopifyProductsAndUpdate
+    , getAllWebHooks
+ } = require('./shopify.js');
 
 const app = express();
 var jsonParser = bodyParser.json();
@@ -17,12 +26,12 @@ app.get('/android/token/', getAndroidToken);
 app.post('/subscriptions/', jsonParser, getSubscriptions);
 
 
-app.post('/subscriptions/shopify/create/webhooks', jsonParser, createShopifyWebhooks);
-
-app.post('/subscriptions/shopify/created', jsonParser, shopifySubscriptionCreated);
-app.post('/subscriptions/shopify/updated', jsonParser, shopifySubscriptionUpdated);
-app.post('/subscriptions/shopify/deleted', jsonParser, shopifySubscriptionDeleted);
-app.post('/subscriptions/getAll', jsonParser, getShopifySubscriptions);
-app.post('/subscriptions/getAllProducts', jsonParser, getShopifyProducts);
-
+app.post('/shopify/charge/create/webhooks', jsonParser, createShopifyWebhooks);
+app.post('/shopify/charge/created', jsonParser, shopifyChargeCreated);
+app.post('/shopify/charge/updated', jsonParser, shopifyChargeUpdated);
+app.post('/shopify/charge/deleted', jsonParser, shopifyChargeDeleted);
+app.post('/shopify/getCharges', jsonParser, getShopifyCharges);
+app.post('/shopify/getAllProducts', jsonParser, getShopifyProducts);
+app.post('/shopify/getAllProductsUpdate', jsonParser, getShopifyProductsAndUpdate);
+app.post('/shopify/getAlldWebHooks', jsonParser, getAllWebHooks);
 app.listen(8100);
