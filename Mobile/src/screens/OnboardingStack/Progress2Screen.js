c5e4bb5ec0d00390e2b5f1dc3ffda9056b8556a2
James Patrick <jamespatrick.apal@fligno.com>
2021-08-18 10:33:37 +0800

    issue #82 IOS fixed

--- Mobile/src/screens/OnboardingStack/Progress2Screen.js
+++ Mobile/src/screens/OnboardingStack/Progress2Screen.js
@@ -427,7 +427,7 @@ export default class Progress2Screen extends React.PureComponent {
     // navigation.pop();
     navigation.state.params.progressEdit !== undefined
       ? navigation.navigate("ProgressEdit")
-      : navigation.pop();
+      : navigation.navigate("ProgressHome")
   };
 
   render() {
