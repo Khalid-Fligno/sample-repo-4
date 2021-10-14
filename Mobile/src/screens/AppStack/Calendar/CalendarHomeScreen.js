62a15014e68f32ff2ad799cfac777a31e9c1e30c
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2021-10-14 18:07:21 +0800

    Oct 11 to Oct 14 Enhancement

--- Mobile/src/screens/AppStack/Calendar/CalendarHomeScreen.js
+++ Mobile/src/screens/AppStack/Calendar/CalendarHomeScreen.js
@@ -235,15 +235,17 @@ class CalendarHomeScreen extends React.PureComponent {
     // console.log("line195",workout)
     if (workout && workout.newWorkout) {
       // console.log("Here....");
+      let warmUpExers = workout.warmUpExercises.filter((warmUpExercise) => {return warmUpExercise});
       const warmUpExercises = await downloadExerciseWC(
         workout,
-        Object.prototype.toString.call(workout.warmUpExercises).indexOf("Array")>-1 ? workout.warmUpExercises : workout.warmUpExercises.filter((warmUpExercise) => {return warmUpExercise}),
+        workout.warmUpExercises,
         workout.warmUpExerciseModel,
         "warmUp"
       );
       if (warmUpExercises.length > 0) {
         const coolDownExercises = await downloadExerciseWC(
           workout,
+          warmUpExers,
           workout.coolDownExercises,
           workout.coolDownExerciseModel,
           "coolDown"
@@ -507,6 +509,10 @@ class CalendarHomeScreen extends React.PureComponent {
     });
   }
 
+  getToFilter() {
+    this.props.navigation.navigate('FilterRecipe')
+  }
+
   openLink = (url) => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     Linking.openURL(url);
@@ -554,6 +560,7 @@ class CalendarHomeScreen extends React.PureComponent {
         <TodayMealsList
           data={todayRecommendedMeal[0]}
           onPress={(res) => this.goToRecipe(res)}
+          filterPress={() => this.getToFilter()}
         />
       </>
     );
