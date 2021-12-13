9723b21e9e7dbdecc56b0d438753eb0f404f02d0
James Patrick Apal <jamespatrick.apal@fligno.com>
2021-12-13 14:49:03 +0800

    back to null lifestyle

--- Mobile/src/screens/AppStack/Nutrition/RecipeSelectionScreen.js
+++ Mobile/src/screens/AppStack/Nutrition/RecipeSelectionScreen.js
@@ -150,7 +150,7 @@ export default class RecipeSelectionScreen extends React.PureComponent {
       onPress={() =>
         this.props.navigation.push("Recipe", {
           recipe: item,
-          backTitle: this.props.navigation.getParam("meal", null),
+          title: this.props.navigation.getParam("meal", null),
         })
       }
       // image={
