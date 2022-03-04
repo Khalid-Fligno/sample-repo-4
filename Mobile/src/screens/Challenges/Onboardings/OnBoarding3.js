109c9e2dcefce48f6af4f31e5b01c3d2579d2f39
Alvin <mcalvin.neri@fligno.com>
2022-03-04 20:39:58 +0800

    add remove listener onboarding2

--- Mobile/src/screens/Challenges/Onboardings/OnBoarding3.js
+++ Mobile/src/screens/Challenges/Onboardings/OnBoarding3.js
@@ -102,7 +102,7 @@ export default class OnBoarding3 extends Component {
 
   // and don't forget to remove the listener
   componentWillUnmount() {
-    // this.focusListener.remove();
+    this.focusListener.remove();
   }
 
   goToScreen(type) {
