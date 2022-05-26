c412c7cf3dc5d0c32e53571fa4760fd84adc5d11
Devyish Gandhi <devyish@domesticcat.com.au>
2022-05-26 16:17:09 +1200

    Get application running

--- Mobile/App.js
+++ Mobile/App.js
@@ -73,12 +73,6 @@ const App = () => {
 
   useEffect(() => {
     LogBox.ignoreLogs(["Setting a timer"]);
-    const _console = _.clone(console);
-    console.warn = (message) => {
-      if (message.indexOf("Setting a timer") <= -1) {
-        _console.warn(message);
-      }
-    };
 
     OneSignal.init("7078b922-5fed-4cc4-9bf4-2bd718e8b548", {
       kOSSettingsKeyAutoPrompt: true,
