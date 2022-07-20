f134d6d383e9527967c19b6b4dbab77f6f719286
Devyish Gandhi <devyish@domesticcat.com.au>
2022-07-21 11:52:27 +1200

    Updated API to contain new credentials to access firebase services

--- Mobile/subscription_service/firebase.js
+++ Mobile/subscription_service/firebase.js
@@ -2,17 +2,17 @@
 
 const firebaseConfiguration = {
   credentials: {
-  "type": "service_account",
-  "project_id": "quickstart-1588594831516",
-  "private_key_id": "f8b8759ce84c3170758f504b82c685444212eacb",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCziCYx8jJbvPYk\nhwW2QOA3iYDRH4KGGgdpQV43gQuzDBzNAg0o9dYSah0E/b+qNjlpXZKpKl+ohb+b\nCVjTwUhFEbGLXPCObuqftl947VR024M8rqpKRZ8c20SSgfARy9ZVwVRuwnpTMw9X\nwd9+6EbqiEod8lr4TpZ3eJHFLO9Eex7Vp6tR/0pK57isLxyAnuuqeAZPGARp4iOD\nHDeM9UViAGcOBmQk7czQTr7fw2Clj10WnOkhgEwLC28928QVVkRCZSkJKsiNU/l8\n30Cr/rNqpe2ZgxXwedalSs75Z3vEImJKB6HecDMFcVP9ObZkOaClzPmrXu5paGWj\nSrC5IJ6LAgMBAAECggEAC3TGqy0/VWdLda1ACg89v42AsNeH5rLGgkrvji8n5fcB\n53sYB+MYE/i9fVIL5vqq0zRrZWwUypYuUeBY0LpmuDGW2MPHfe9J/YMPuJG8b/CK\nAVdL+o1Sx+6eA5NTFG+jb7l0f9TdJW3+rqTqdBiyXcjwodSTRYEOTPVep+xkIER3\nWtBcl7b7eoJtuiY/0AbjX01AncvhJlA0xbLHJMsrt7BWXIPIBnyco3q3lF/4Cy1N\nTeVYxA48oDNfcX5HcOKiNK6E84BT6KBh62hKph0XaTvPL3HQk7pfrTabmhmwj92U\nWNdryb2N8kdlaSWY1Cly9+ewn63L387UfCjZq8f6EQKBgQD44oJqXRNA83Hzk3Ve\n1NoDwIjAZ8VhJdD3dEOLZz/6IV4tX9eVbGGDNU0FaWrbfVgBH9A2/zXMnMP7N1Fy\nBcrpvsWDrsRC8Hf7Y1Uf3E6nTug1SDHX79faMGRnMqVi2tocOLaC0HqWcxHELdMl\nxcT0eLZtPjdBYYamzgTR0rnK/wKBgQC4qhKO8IpjHREeT/FTgfpSanKFv2Fk0KfQ\n+fA/8iVodeMcH2vd4r9Fj0r+sMWznDJuvq/iaGG3uLdgI5K2A6oEPki+U9VbUO9s\n6UoYGA80Ps27i+ZcJepg38N2jAkQ268Z957yqfmMa9knON+G1uD0FO3enX0otXGV\nBim46IEodQKBgQC1HcDiPlFglsh6BPkIxcTRri7WNa4BgE+NhGIS9FQuZlHumyx7\nZJ+hqAwgwBIvIAwG5WvUWeyAtiQ4kI0IeQatC1MY/Pl97RijLCoqQVxLjLSW8a60\nAaoG8ehe4RBR3AmVOAA5OhW7nolzRhW+CynE7oNzBC+ZuKsQgCQpvTvPaQKBgGxI\n3oUPLDceblFWE28igsUdQnaHWPcjG6UBSw5UUfdFWZ2+FySPuBV0B0f+JJBEAZbL\nHpwnFVYEbRNlCLzOvT2rwEBLLItVlHBLBHXIdfHu+fkF+lkCqsOPszCqwQ4vpCgX\n+g7V+U2MWFQVl42cJ2E5fJrTwvbCHw6CBKk3PbWZAoGAQYTrNdeF+pnczP/iOOOh\nxD43O7iD6lO1unpVhRWPsSH1S57/rZZD6QOBtWtio39gA1mxiexuwwek7HITDf6z\naF7xJIZmrG7FoGMUhgkAzJnO5xJpi3twfRpoK/XO7pSL/ofo0y25Pa2+elvu60dZ\nJFE5Xoag9lD4mmSvRAid9mA=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-p4vxt@quickstart-1588594831516.iam.gserviceaccount.com",
-  "client_id": "104182960927988919857",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-p4vxt%40quickstart-1588594831516.iam.gserviceaccount.com"
-},
+    "type": "service_account",
+    "project_id": "fitazfk-app",
+    "private_key_id": "caeeb652cd978f8d85b84cf4471210c7b20f744e",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCwqVxVHn5kOkqH\nGNZ9gk3AbWXuyaipl66/SCHlaZ56eDacMmjPvOSEt+UgVuBvqsoem7TsC9pz6G3J\njsUfdfi6HZN5EE79hNc4q4dC6SJEucxFXtIsBare9bSRAfx/DXSSd8oOSZHn4Jl6\nm2B/Nw4tmwtxOmPk8TdnCI60ylLB4WhDJBjiepn1+U4pNh92okpFPoMUMgGuXh/9\nSDBcy0lkOJAry8CYeWra3AV0+Y+hvnCCTe39IQ7I6JVsC8SaaS1jqj/HL46jdrQn\nlIJFfooTZ43Yzck3Z0GApCrd79M2W/O8e2Qpbua23IZ4eL4nU40cdTOk0deCgDly\nPUv/NkZRAgMBAAECggEAAZmWZlzItoHqzvXVayIpBTErHd+PQrJG26uMsgoT4eZQ\n5qHUE5iRexcQG6E/IQfGEu2kVMnH3FT1L0DW+HGuM1+lSFCytHFVl9PsMqRk95l1\nWAX+MjZJbZbtABi0DOxbZfWw25JZ6ySRuJvBDZnl1m7RFVZllUsuziTfJ6mYQwen\n8CXsbO78zi3GZoZvy5ie7GLh6gR31m1P95lRaVPoahQo2oCT35GxeakxcMwY+N9e\nczk4pWGqfblFtLRoegrR5QQcWEceu+zJ/olr8QyRB+Qca7hKocJU7hRtzj4H8mgk\nBcWGHTjkQViYERStZ5qE+3lthvoaGTFRN+t28J21uQKBgQDVwM/cL+E76B9YW7UC\nqCc/WuqUM2szckysBxOxLrf1YiUQDN9CsPMqwWBGdfAbKQMzh6x2N8xmzukVTZ+P\n5IicxfW2aSg5bpACEptLm5dNz9PUIDWmOXzOKKgGwCdPBQoWikjE4cTgzrc0oFvk\na0Dp43W0r889ouUCG0zSnSvESQKBgQDTk9a69AQROjALRlK7/9kKqSPf00uIGi6B\nEKlXWMf6bWeCWe9d8TM9fBc5p2kpSv3FszqjjuxMXvRe5mSm0Cq9y2dbNhybCpZB\nXqNI08XPd29KA1W5n2u+aWNTysE6cpbi0q29BiN7djzBfvErM+tsW/RxZNfec0tR\njBdVo63hyQKBgQCp8MlRPZ/MiReioAahPh8194pYmSi5lj3u5xF45LPMm2LsB8/Y\nreYmL+fjHRDwBwNMjKlIs8QGWKpnDXe2ZvNV8O0RQTmBr60+eJyO1/zLGFhJZfJB\nXaMkJJ6x/0NBR7hlgzuni/eC49Y0OOnWCI9rZDPIm54gWsnHivzg1aReQQKBgGv/\nofNqTY0xo4aNzfuS/IoZjmtZIswFeUl+ZMJmWiX1pImAONQ+I8a1yfEvCFoAootB\nEqScdvgA3EJ43lXcGoUpB8giVfW6MLqLgVPSbb2aHJ+uFhsbSlc22nHSJFIeXfDI\nU9ESChiiYR7wGlDQPtLgGWWscRhmx8KHfP3FACQpAoGBAJ9VIY5lNYRVNv7/tz25\nlVNQxfpa1zRWT59FWDNOjI2rJzASvBt1gw+kQbUbpDq+BOreOFp0p0Vvqoc+gcOW\nZJMYWBAefM4SNoEbSch8xYyWFXJUg7ue/DbSNqe7Inu/HDGvVl4N3GlEIgBMAbZ5\nUt17QtxQHZSpdbAFVMpcnJ2J\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-hep4p@fitazfk-app.iam.gserviceaccount.com",
+    "client_id": "102065208971400800529",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hep4p%40fitazfk-app.iam.gserviceaccount.com"
+  },
   options: {
     databaseURL : "https://fitazfk-app.firebaseio.com",
     storageBucket: "fitazfk-app.appspot.com"
